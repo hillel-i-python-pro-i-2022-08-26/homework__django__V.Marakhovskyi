c2eb7c27d76e43b5f618d3d540a99322186f9c0a
vladyslavius <marakhovskyiv@gmail.com>
2022-10-02 19:42:57 +0300

    🎉 first commit in new branch - 'templates'

--- apps/greetings/views/greetings_view.py
+++ apps/greetings/views/greetings_view.py
@@ -3,6 +3,9 @@ from django.http import HttpRequest, HttpResponse
 from apps.greetings.services.generate_random_name import generate_name
 
 
+# render
+
+
 def greetings_view(request: HttpRequest, name: str | None = None) -> HttpResponse:
     if name is None:
         name = generate_name()
