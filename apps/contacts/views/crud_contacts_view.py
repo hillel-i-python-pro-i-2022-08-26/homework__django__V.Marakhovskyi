9dbd5b7a81a3301ddf534c7ec334a832521e03b9
vladyslavius <marakhovskyiv@gmail.com>
2022-10-15 11:52:52 +0300

    :sparkles: Updated view

--- apps/contacts/views/crud_contacts_view.py
+++ apps/contacts/views/crud_contacts_view.py
@@ -1,8 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.utils import timezone
 
 from apps.contacts.forms import ContactForm
 
 
 def crud_contacts(request):
-    form = ContactForm()
+    if request.method == "POST":
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect("contacts:index")
+    else:
+        form = ContactForm()
     return render(request, "contacts/form.html", {"form": form})
