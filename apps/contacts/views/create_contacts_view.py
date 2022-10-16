a8537f0bf40fbadca1c0c70f315791b98a8d0e5b
vladyslavius <marakhovskyiv@gmail.com>
2022-10-16 18:49:20 +0300

    :rocket: Edit a CRUD functions (added HttpRequest & HttpResponse)

--- apps/contacts/views/create_contacts_view.py
+++ apps/contacts/views/create_contacts_view.py
@@ -1,10 +1,11 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render, redirect
 from django.utils import timezone
 
 from apps.contacts.forms import ContactForm
 
 
-def crud_contacts(request):
+def crud_contacts(request: HttpRequest) -> HttpResponse:
     if request.method == "POST":
         form = ContactForm(request.POST)
         if form.is_valid():
--- apps/contacts/views/delete_contacts_view.py
+++ apps/contacts/views/delete_contacts_view.py
@@ -1,9 +1,10 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import redirect
 
 from apps.contacts.models import Contact
 
 
-def contact_delete(request, id):
+def contact_delete(request: HttpRequest, id: int) -> HttpResponse:
     contact = Contact.objects.get(pk=id)
     contact.delete()
     return redirect("contacts:index")
