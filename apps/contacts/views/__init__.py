fa5636ce1f5f3f77013d35612c9b849b2efabc9c
vladyslavius <marakhovskyiv@gmail.com>
2022-10-16 18:38:59 +0300

    :rocket: Applied correct names for views and changed 'update' function

--- apps/contacts/views/__init__.py
+++ apps/contacts/views/__init__.py
@@ -1,4 +1,4 @@
-from apps.contacts.views.contact_list_view import get_contacts
-from apps.contacts.views.crud_contacts_view import crud_contacts
+from apps.contacts.views.create_contacts_view import crud_contacts
 from apps.contacts.views.delete_contacts_view import contact_delete
+from apps.contacts.views.read_contact_view import get_contacts
 from apps.contacts.views.update_contacts_view import update_contact
--- apps/contacts/views/update_contacts_view.py
+++ apps/contacts/views/update_contacts_view.py
@@ -1,23 +1,18 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import redirect, render
 
 from apps.contacts.forms import ContactForm
 from apps.contacts.models import Contact
 
 
-def update_contact(request, id=0):
-    if request.method == "GET":
-        if id == 0:
-            form = ContactForm()
-        else:
-            contact = Contact.objects.get(pk=id)
-            form = ContactForm(instance=contact)
-        return render(request, "contacts/form.html", {"form": form})
-    else:
-        if id == 0:
-            form = ContactForm(request.POST)
-        else:
-            contact = Contact.objects.get(pk=id)
-            form = ContactForm(request.POST, instance=contact)
+def update_contact(request: HttpRequest, id: int) -> HttpResponse:
+    contact = Contact.objects.get(pk=id)
+    if request.method == "POST":
+        form = ContactForm(request.POST, instance=contact)
         if form.is_valid():
             form.save()
-        return redirect("contacts:index")
+            return redirect("contacts:index")
+        return render(request, "contacts/form.html", {"form": form})
+    else:
+        form = ContactForm(instance=contact)
+    return render(request, "contacts/form.html", {"form": form})
