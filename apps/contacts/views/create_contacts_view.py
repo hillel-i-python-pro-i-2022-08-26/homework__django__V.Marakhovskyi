6682316e1a2e011bbbde1ef6b26f5d21fd702ff0
vladyslavius <marakhovskyiv@gmail.com>
2022-10-16 20:49:25 +0300

    :white_check_mark: Modified 'Create' operation

--- apps/contacts/views/create_contacts_view.py
+++ apps/contacts/views/create_contacts_view.py
@@ -1,6 +1,5 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render, redirect
-from django.utils import timezone
 
 from apps.contacts.forms import ContactForm
 
@@ -9,10 +8,7 @@ def crud_contacts(request: HttpRequest) -> HttpResponse:
     if request.method == "POST":
         form = ContactForm(request.POST)
         if form.is_valid():
-            post = form.save(commit=False)
-            post.author = request.user
-            post.published_date = timezone.now()
-            post.save()
+            form.save()
             return redirect("contacts:read")
     else:
         form = ContactForm()
