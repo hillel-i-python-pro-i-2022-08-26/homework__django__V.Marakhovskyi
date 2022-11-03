2f65ffd970d2e0814e14cc86c7dc0774a2565a17
vladyslavius <marakhovskyiv@gmail.com>
2022-11-03 15:07:54 +0200

    :rocket:  Add static folder and create first CSS style for User App

--- core/settings.py
+++ core/settings.py
@@ -158,6 +158,10 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
+STATICFILES_DIRS = [
+    APPS_DIR / "static",
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
