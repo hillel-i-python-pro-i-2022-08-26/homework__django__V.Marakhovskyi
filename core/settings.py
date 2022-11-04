bb91b8eaa29f9912760e60b957ed8ef3a05161e8
vladyslavius <marakhovskyiv@gmail.com>
2022-11-04 19:22:46 +0200

    :rocket:  intermediate commit

--- core/settings.py
+++ core/settings.py
@@ -158,7 +158,7 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
-STATIC_ROOT = BASE_DIR / "staticfiles"
+# STATIC_ROOT = BASE_DIR / "staticfiles"
 
 STATICFILES_DIRS = [
     APPS_DIR.joinpath("static"),
