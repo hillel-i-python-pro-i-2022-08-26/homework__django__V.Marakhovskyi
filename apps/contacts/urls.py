4d2fac6c58343fd5050d407ff6a1474b67770545
vladyslavius <marakhovskyiv@gmail.com>
2022-11-12 19:56:42 +0200

    :sparkles:  Apply ClassBasedView for Delete-operation

--- apps/contacts/urls.py
+++ apps/contacts/urls.py
@@ -8,7 +8,8 @@ urlpatterns = [
     path("", views.ArticleListView.as_view(), name="read"),
     path("form/", views.ContactCreateView.as_view(), name="create"),
     # path("form/", views.crud_contacts, name="create"),
-    path("delete/<int:pk>/", views.contact_delete, name="delete"),
+    # path("delete/<int:pk>/", views.contact_delete, name="delete"),
+    path("delete/<int:pk>/", views.ContactDeleteView.as_view(), name="delete"),
     # path("update/<int:pk>/", views.update_contact, name="update"),
     path("update/<int:pk>/", views.ContactUpdateView.as_view(), name="update"),
 ]
--- apps/contacts/views/delete_contacts_view.py
+++ apps/contacts/views/delete_contacts_view.py
@@ -1,10 +1,15 @@
-from django.http import HttpRequest, HttpResponse
-from django.shortcuts import redirect
+from django.urls import reverse_lazy
+from django.views.generic import DeleteView
 
 from apps.contacts.models import Contact
 
 
-def contact_delete(request: HttpRequest, pk: int) -> HttpResponse:
-    contact = Contact.objects.get(pk=pk)
-    contact.delete()
-    return redirect("contacts:read")
+# def contact_delete(request: HttpRequest, pk: int) -> HttpResponse:
+#     contact = Contact.objects.get(pk=pk)
+#     contact.delete()
+#     return redirect("contacts:read")
+
+
+class ContactDeleteView(DeleteView):
+    model = Contact
+    success_url = reverse_lazy("contacts:read")
