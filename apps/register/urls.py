6d8a2bb83ed3c91a2fa2ff9a561be0dff331e545
vladyslavius <marakhovskyiv@gmail.com>
2022-11-15 00:32:19 +0200

    :rocket: Transform register view from function to ClassBased

--- apps/register/urls.py
+++ apps/register/urls.py
@@ -5,7 +5,8 @@ from . import views
 app_name = "auth"
 
 urlpatterns = [
-    path("register/", views.register, name="register"),
+    # path("register/", views.register, name="register"),
+    path("register/", views.RegisterUser.as_view(), name="register"),
     path("login/", views.user_login, name="login"),
     path("logout/", views.user_logout, name="logout"),
 ]
--- apps/register/views.py
+++ apps/register/views.py
@@ -1,23 +1,35 @@
-from django.contrib import messages
 from django.contrib.auth import login, logout
 from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
 
 from apps.register.forms import SignUpForm, UserLoginForm
 
 
-def register(request):
-    if request.method == "POST":
-        form = SignUpForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            login(request, user)
-            messages.success(request, "Registration successful")
-            return redirect("index")
-        else:
-            messages.error(request, "Registration failed")
-    else:
-        form = SignUpForm()
-    return render(request, "auth/register.html", {"form": form})
+# def register(request):
+#     if request.method == "POST":
+#         form = SignUpForm(request.POST)
+#         if form.is_valid():
+#             user = form.save()
+#             login(request, user)
+#             messages.success(request, "Registration successful")
+#             return redirect("index")
+#         else:
+#             messages.error(request, "Registration failed")
+#     else:
+#         form = SignUpForm()
+#     return render(request, "auth/register.html", {"form": form})
+
+
+class RegisterUser(CreateView):
+    form_class = SignUpForm
+    template_name = "auth/register.html"
+    success_url = reverse_lazy("index")
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["title"] = "Register"
+        return context
 
 
 def user_login(request):
